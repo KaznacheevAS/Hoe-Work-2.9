1923d3445d593fd62be4483281a51c5178baba42
Kaznacheev <antkaznacheev@gmail.ru>
2021-09-01 12:24:31 +0300

    Lessons 9

---
+++ Hoe Work 2.9/Animates.swift
@@ -0,0 +1,26 @@
+//
+//  Animates.swift
+//  Hoe Work 2.9
+//
+//  Created by Kaznacheev on 31.08.2021.
+//
+
+import Spring
+
+class Animates {
+    
+    static let animate = Animates()
+    
+    let animate: [String] = [
+        "slideLeft",
+        "slideRight",
+        "slideDown",
+        "slideUp",
+        "squeezeLeft",
+        "squeezeRight",
+        "squeezeDown",
+        "squeezeUp",
+        "swing"
+    ]
+    
+}
--- Hoe Work 2.9/ViewController.swift
+++ Hoe Work 2.9/ViewController.swift
@@ -5,15 +5,29 @@
 //  Created by Kaznacheev on 30.08.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgAnimateView: SpringView!
+    @IBOutlet weak var bottom: SpringButton!
+    
+   // private var animate = Animates()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        bottom.setTitle("Run Animation", for: .normal)
     }
 
-
+    @IBAction func runSpringBottom(_ sender: SpringButton) {
+        bgAnimateView.animation = "slideLeft"
+        bgAnimateView.curve = "easeInOut"
+        bgAnimateView.force = 2
+        bgAnimateView.duration = 1
+        bgAnimateView.delay = 0.3
+        
+        bgAnimateView.animate()
+        
+        //bottom.setTitle("Run ", for: .normal)
+    }    
 }
 
